24970b3167f1cf06b8817154e8972f2acc513bab
Chandramouli Veerubhotla <chandramouli.veerubhotla@gmail.com>
2024-07-26 18:13:50 +0530

    Modified couple of components

--- src/app/pages/new-fund-page/new-fund-page.component.ts
+++ src/app/pages/new-fund-page/new-fund-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-fund-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule, MatFormField, MatInputModule],
   templateUrl: './new-fund-page.component.html',
   styleUrl: './new-fund-page.component.scss'
 })
 export class NewFundPageComponent {
+
+  form: FormGroup = new FormGroup({
+    name: new FormControl<string>('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    description: new FormControl<string|null>(null)
+  })
 
 }
