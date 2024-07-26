1d280e2165819adf16258378ddc2fb7990b57a0d
Chandramouli Veerubhotla <chandramouli.veerubhotla@gmail.com>
2024-07-26 18:19:38 +0530

    Removed dialog way for creating new fund

--- src/app/pages/my-funds-page/my-funds-page.component.ts
+++ src/app/pages/my-funds-page/my-funds-page.component.ts
@@ -2,22 +2,14 @@ import { Component } from '@angular/core';
 import { FundItemComponent } from '../../components/fund-item/fund-item.component';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
-import { NewFundPageComponent } from '../new-fund-page/new-fund-page.component';
 
 @Component({
   selector: 'app-my-funds-page',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, FundItemComponent, MatBottomSheetModule],
+  imports: [RouterLink, NavbarComponent, FundItemComponent],
   templateUrl: './my-funds-page.component.html',
   styleUrl: './my-funds-page.component.scss'
 })
 export class MyFundsPageComponent {
-
-  constructor(private _bs: MatBottomSheet) { }
-
-  newFundDialog() {
-    this._bs.open(NewFundPageComponent)
-  }
 
 }
